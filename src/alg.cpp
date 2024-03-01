89c0c0576129d75940314a4bd2cbf33d15539a7e
BaevGeorgy55 <160393440+BaevGeorgy55@users.noreply.github.com>
2024-03-01 21:05:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,72 +5,62 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
-  for (int i = 2; i < value; i++)
-    {
-        if (value % i == 0)
-            return false;
-    }
-    return true;
+  for (int i = 2; i < value; i++) {
+    if (value % i == 0)
+      return false;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
-    int size = 100;
-    int cntr = 0;
-    int arr[size] {0};
-    int prost[size] {0};
-    for (int i = 0; i < size; i++) {
-        arr[i] = i + 1;
-    }
-    for (int i = 0; i < size; i++) {
-        if (checkPrime(arr[i]) && arr[i] != 1) {
-            prost[cntr++] = arr[i];
-        }
-    }
-    for (int i = 0; i < size; i++) {
-        if (n == i) {
-            return prost[i-1];
-        }
-    }
+int size = 100;
+int cntr = 0;
+int arr[size] {0};
+int prost[size] {0};
+for (int i = 0; i < size; i++) {
+  arr[i] = i + 1;
+}
+  for (int i = 0; i < size; i++) {
+    if (checkPrime(arr[i]) && arr[i] != 1)
+      prost[cntr++] = arr[i];
+  }
+  for (int i = 0; i < size; i++) {
+    if (n == i)
+      return prost[i-1];
+  }
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
-    int size = 10;
-    int cntr = 0;
-    int arr[size] {0};
-    int prost[size] {0};
-    for (int i = 0; i < size; i++) {
-        arr[i] = i + 1;
-    }
-    for (int i = value + 1; i < size; i++) {
-        if (checkPrime(arr[i]) && arr[i] != 1) {
-            return arr[i];
-        }
-    }
+int size = 10;
+int cntr = 0;
+int arr[size] {0};
+int prost[size] {0};
+for (int i = 0; i < size; i++) {
+  arr[i] = i + 1;
+}
+  for (int i = value + 1; i < size; i++) {
+    if (checkPrime(arr[i]) && arr[i] != 1)
+      return arr[i];
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
-    int size = 15;
-    int arr[size] {0};
-    int prost[size] {0};
-    int cntr = 0, a = 0;
-
-    for (int i = 0; i < size; i++)
-    {
-        arr[i] = i + 1;
-    }
-    for (int i = 0; i < size; i++)
-    {
-        if (checkPrime(arr[i]) && (arr[i] != 1))
-        {
-            prost[cntr++] = arr[i];
-        }
-    }
-    for (int i = 0; i < hbound; i++)
-    {
-        a += prost[i];
-    }
+int size = 15;
+int arr[size] {0};
+int prost[size] {0};
+int cntr = 0, a = 0;
+  for (int i = 0; i < size; i++){
+    arr[i] = i + 1;
+  }
+  for (int i = 0; i < size; i++){
+    if (checkPrime(arr[i]) && (arr[i] != 1))
+      prost[cntr++] = arr[i];
+  }
+  for (int i = 0; i < hbound; i++){
+    a += prost[i];
+  }
   return a;
 }
