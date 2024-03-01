6da17ab8339fc88609e309cc0a6cb7f0972beb79
BaevGeorgy55 <160393440+BaevGeorgy55@users.noreply.github.com>
2024-03-01 22:45:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,36 +14,36 @@ bool checkPrime(uint64_t value) {
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
-  uint64_t size = 100;
-  uint64_t cntr = 0;
-  uint64_t arr[size] {0};
-  uint64_t prost[size] {0};
-  for (uint64_t i = 0; i < size; i++) {
-    arr[i] = i + 1;
-  }
-  for (uint64_t i = 0; i < size; i++) {
-    if (checkPrime(arr[i]) && arr[i] != 1)
-      prost[cntr++] = arr[i];
-  }
-  for (uint64_t i = 0; i < size; i++) {
-    if (n == i)
-      return prost[i-1];
-  }
+    uint64_t size = 100;
+    uint64_t cntr = 0;
+    uint64_t arr[size] {0};
+    uint64_t prost[size] {0};
+    for (uint64_t i = 0; i < size; i++) {
+      arr[i] = i + 1;
+    }
+    for (uint64_t i = 0; i < size; i++) {
+      if (checkPrime(arr[i]) && arr[i] != 1)
+        prost[cntr++] = arr[i];
+    }
+    for (uint64_t i = 0; i < size; i++) {
+      if (n == i)
+        return prost[i-1];
+    }
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
-  uint64_t size = 10;
-  uint64_t cntr = 0;
-  uint64_t arr[size] {0};
-  uint64_t prost[size] {0};
-  for (uint64_t i = 0; i < size; i++) {
-    arr[i] = i + 1;
-  }
-  for (uint64_t i = value + 1; i < size; i++) {
-    if (checkPrime(arr[i]) && arr[i] != 1)
-      return arr[i];
-  }
+    uint64_t size = 10;
+    uint64_t cntr = 0;
+    uint64_t arr[size] {0};
+    uint64_t prost[size] {0};
+    for (uint64_t i = 0; i < size; i++) {
+      arr[i] = i + 1;
+    }
+    for (uint64_t i = value + 1; i < size; i++) {
+      if (checkPrime(arr[i]) && arr[i] != 1)
+        return arr[i];
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
