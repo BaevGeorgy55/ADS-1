0890ff7af2ae42d659d13836a16e6cfbca4c6991
BaevGeorgy55 <160393440+BaevGeorgy55@users.noreply.github.com>
2024-03-01 19:54:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,7 +5,7 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
-  for (int i = 2; i < (value/2); i++)
+  for (int i = 2; i < value; i++)
     {
         if (value % i == 0)
             return false;
