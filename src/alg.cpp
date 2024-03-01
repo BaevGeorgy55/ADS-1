d34689fe2c8833ca82cf7afe4a9d3c029731d697
BaevGeorgy55 <160393440+BaevGeorgy55@users.noreply.github.com>
2024-03-01 22:13:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,18 +14,18 @@ bool checkPrime(uint64_t value) {
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
-  int size = 100;
-  int cntr = 0;
-  int arr[size] {0};
-  int prost[size] {0};
-  for (int i = 0; i < size; i++) {
+  uint64_t size = 100;
+  uint64_t cntr = 0;
+  uint64_t arr[size] {0};
+  uint64_t prost[size] {0};
+  for (uint64_t i = 0; i < size; i++) {
     arr[i] = i + 1;
   }
-  for (int i = 0; i < size; i++) {
+  for (uint64_t i = 0; i < size; i++) {
     if (checkPrime(arr[i]) && arr[i] != 1)
       prost[cntr++] = arr[i];
   }
-  for (int i = 0; i < size; i++) {
+  for (uint64_t i = 0; i < size; i++) {
     if (n == i)
       return prost[i-1];
   }
@@ -33,14 +33,14 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
-  int size = 10;
-  int cntr = 0;
-  int arr[size] {0};
-  int prost[size] {0};
-  for (int i = 0; i < size; i++) {
+  uint64_t size = 10;
+  uint64_t cntr = 0;
+  uint64_t arr[size] {0};
+  uint64_t prost[size] {0};
+  for (uint64_t i = 0; i < size; i++) {
     arr[i] = i + 1;
   }
-  for (int i = value + 1; i < size; i++) {
+  for (uint64_t i = value + 1; i < size; i++) {
     if (checkPrime(arr[i]) && arr[i] != 1)
       return arr[i];
   }
@@ -48,18 +48,18 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
-  int size = 15;
-  int arr[size] {0};
-  int prost[size] {0};
-  int cntr = 0, a = 0;
-  for (int i = 0; i < size; i++) {
+  uint64_t size = 15;
+  uint64_t arr[size] {0};
+  uint64_t prost[size] {0};
+  uint64_t cntr = 0, a = 0;
+  for (uint64_t i = 0; i < size; i++) {
     arr[i] = i + 1;
   }
-  for (int i = 0; i < size; i++) {
+  for (uint64_t i = 0; i < size; i++) {
     if (checkPrime(arr[i]) && (arr[i] != 1))
       prost[cntr++] = arr[i];
   }
-  for (int i = 0; i < hbound; i++) {
+  for (uint64_t i = 0; i < hbound; i++) {
     a += prost[i];
   }
   return a;
